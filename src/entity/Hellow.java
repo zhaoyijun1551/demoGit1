87b68dd5c9bcf6f36930eb2d94100d87b9349e0d
zhaoyijun1551 <1357130406@qq.com>
2019-06-10 16:44:54 +0800

    first commit

---
+++ src/entity/Hellow.java
@@ -0,0 +1,17 @@
+package entity;
+
+import org.junit.Test;
+
+public class Hellow {
+    /**
+     * junit测试
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+    }
+
+
+
+}
